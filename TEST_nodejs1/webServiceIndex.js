e81506ee6f04389bf5e703311deca66109199076
MarcusJones <jones.0bj3@gmail.com>
2018-11-04 08:52:47 +0100

    add Part 4.1.10 examples

---
+++ TEST_nodejs1/webServiceIndex.js
@@ -0,0 +1,18 @@
+// Code snippet from https://www.w3schools.com/nodejs/nodejs_http.asp
+// Http library
+const http = require('http');
+// File service library
+const fs = require('fs');
+// Http port
+const port = 3000;
+// Filename for file service to load
+const filename = "index.html";
+// Configure web service
+const app = http.createServer(function (request, response){
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.end(fs.readFileSync(__dirname + "/" + filename));
+});
+// Notify console
+console.log("Web Server started on port 3000\nhttp://localhost:"+port);
+// Start server with http port
+app.listen(port);
