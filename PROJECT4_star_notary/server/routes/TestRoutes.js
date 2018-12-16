6228797c0627471bfe5040410b1868f70cd7f39d
MarcusJones <jones.0bj3@gmail.com>
2018-12-16 17:41:19 +0100

    refactor of test routes finished

--- PROJECT4_star_notary/server/routes/TestRoutes.js
+++ PROJECT4_star_notary/server/routes/TestRoutes.js
@@ -5,8 +5,7 @@ controller = require('../controllers/TestController')
 /**
  * Controller Definition 
  */
-class TestController {
-
+class TestRoutes {
     /**
      * Constructor 
      * @param {*} server 
@@ -14,36 +13,16 @@ class TestController {
     constructor(server) {
         this.server = server;
         this.blocks = [];
-        // this.helloGet(); // 1
-        this.server.route({ method:'GET', path:'/api/hello', handler:controller.helloGet }) 
-        this.server.route({ method:'POST', path:'/api/hello', handler:controller.helloPost });
-        this.server.route({ method:'GET', path:'/api/greet/{user}', handler:controller.helloGreetUser });
-        this.server.route({ method:'GET', path:'/api/number/{index}', handler:controller.helloNumberIndex });
-        // this.helloPost(); // 2
-        // this.helloNumberIndex(); // 3
-        // this.helloGreetUser(); // 4
+
+        this.server.route({ method:'GET',   path:'/api/hello',          handler:controller.helloGet }) 
+        
+        this.server.route({ method:'POST',  path:'/api/hello',          handler:controller.helloPost });
+        
+        this.server.route({ method:'GET',   path:'/api/greet/{user}',   handler:controller.helloGreetUser });
+        
+        this.server.route({ method:'GET',   path:'/api/number/{index}', handler:controller.helloNumberIndex });
+        
     }
-
-    // helloGet(){ 
-    //     this.server.route({ method:'GET', path:'/api/hello', handler:controller.helloGet }) 
-    // }
-
-    // helloPost(){
-    //     this.server.route({ method:'POST', path:'/api/hello', handler:controller.helloPost });
-    // }
-
-    // helloGreetUser(){
-    //     this.server.route({ method:'GET', path:'/api/greet/{user}', handler:controller.helloGreetUser });
-    // }
-
-    // helloNumberIndex(){
-    //     // Test route 4
-    //     this.server.route({ method:'GET', path:'/api/number/{index}', handler:controller.helloNumberIndex });
-    // }
 }
 
-/**
- * Exporting the Testontroller class
- * @param {*} server 
- */
-module.exports = (server) => { return new TestController(server);}
+module.exports = (server) => { return new TestRoutes(server);}
