b747aa60983a4f42cd475214f2df561f02097e77
MarcusJones <jones.0bj3@gmail.com>
2018-12-16 20:38:37 +0100

    add Error handling and logging.

--- PROJECT4_star_notary/server/controllers/StarController.js
+++ PROJECT4_star_notary/server/controllers/StarController.js
@@ -1,28 +1,50 @@
 var exported = {
-    
-    POST_requestValidation: function RequestValidation_POST(request, h) {
+
+    POST_requestValidation: async function RequestValidation_POST(request, h) {
+        console.log(`POST validation requested: ${JSON.stringify(request.payload)}`);
+        if (!request.payload.hasOwnProperty('address')) {
+            return boom.badRequest('Missing payload key. Pass address as JSON.');
+        }
         return 'POST_requestValidation';
     },
 
-    POST_messageSigValidate: function POST_messageSigValidate(request, h) {
+    POST_messageSigValidate: async function POST_messageSigValidate(request, h) {
+        console.log(`POST validation requested: ${JSON.stringify(request.payload)}`);
+        if (!request.payload.hasOwnProperty('address')) {
+            return boom.badRequest('Missing payload key. Pass address as JSON.');
+        }
+        if (!request.payload.hasOwnProperty('signature')) {
+            return boom.badRequest('Missing payload key. Pass signature as JSON.');
+        }
+        
         return 'POST_messageSigValidate';
     },
 
-    POST_block: function POST_block(request, h) {
+    POST_block: async function POST_block(request, h) {
+        console.log(`POST validation requested: ${JSON.stringify(request.payload)}`);
+        if (!request.payload.hasOwnProperty('address')) {
+            return boom.badRequest('Missing payload key. Pass address as JSON.');
+        }
+        if (!request.payload.hasOwnProperty('star')) {
+            return boom.badRequest('Missing payload key. Pass star as JSON.');
+        }
         return 'POST_block';
     },
 
-    GET_starByHash: function GET_starByHash(request, h) {
+    GET_starByHash: async function GET_starByHash(request, h) {
+        console.log(`GET block by BLOCK_HASH: ${request.params.BLOCK_HASH}`);
         return 'GET_starByHash';
     },
 
-    GET_starByAddress: function GET_starByAddress(request, h) {
+    GET_starByAddress: async function GET_starByAddress(request, h) {
+        console.log(`GET block by BLOCK_ADDRESS: ${request.params.BLOCK_ADDRESS}`);
         return 'GET_starByAddress';
     },
 
-    GET_blockByHeight: function GET_blockByHeight(request, h) {
+    GET_blockByHeight: async function GET_blockByHeight(request, h) {
+        console.log(`GET block by BLOCK_HEIGHT: ${request.params.BLOCK_HEIGHT}`);
         return 'GET_blockByHeight';
-    },        
+    },
 
 }
 module.exports = exported
