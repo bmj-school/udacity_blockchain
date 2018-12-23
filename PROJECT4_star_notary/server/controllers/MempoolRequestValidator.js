594a46e70dc3f194b4fde6ba55198760973c44e9
MarcusJones <jones.0bj3@gmail.com>
2018-12-23 16:23:54 +0100

    update docs

--- PROJECT4_star_notary/server/controllers/MempoolRequestValidator.js
+++ PROJECT4_star_notary/server/controllers/MempoolRequestValidator.js
@@ -1,25 +1,22 @@
 const SHA256 = require('crypto-js/sha256');
 
-// const TimeoutRequestsWindowTime = 5*60*1000;
-const testingTimeStep = 100; // ms, set to 1000 for final production
-const TimeoutRequestsWindowTime = 5 * testingTimeStep; //1000;
-console.log(`Request window: ${TimeoutRequestsWindowTime} s`);
+const testingTimeStep = 100; //[ms] set to 1000 for final production
+const TimeoutRequestsWindowTime = 5 * testingTimeStep; //[ms] 1000;
+// const TimeoutRequestsWindowTime = 5*60*1000; //[ms] 5 minutes window
+console.log(`Request window: ${TimeoutRequestsWindowTime/1000} s`);
 
-
-// let d = Date.now()
-// console.log(d);
 class Request {
     constructor(address) {
         this.address = address
         this.requestTimestamp = Date.now();
     }
 
-    thisResponse(){
+    respond(){
         return {
             'address' : this.address,
             'requestTimestamp' : this.requestTimestamp,
-            'message' : `${this.address}:${requestTimestamp}:${starRegistry}`,
-            'validationWindow' : this.requestTimestamp - Date.now()  
+            'message' : `${this.address}:${this.requestTimestamp}:starRegistry`,
+            'validationWindow' : TimeoutRequestsWindowTime+(this.requestTimestamp - Date.now()) / 1000
         }
     }
 }
@@ -47,22 +44,19 @@ class RequestPool {
     addRequest(address) {
         if (address in this.mempool) {
             console.log('Already in requests!');
-            // Return the Req
-
+            // TODO: Return the request.respond
         } else {
             var self = this // Need to keep the instance in scope, this is not in scope!
             // This is a new Request
-            this.mempool[address] = new Request;
+            this.mempool[address] = new Request(address);
             // Add a countdown to this address key
             this.timeoutRequests[address] = setTimeout(
                 function () {
                     self.removeValidationRequest(address)
                 }, TimeoutRequestsWindowTime);
             console.log(`Added ${address} to mempool`);
+            // TODO: Return the request.respond
         }
-
-
-
     }
 
 
@@ -82,8 +76,6 @@ console.log('**** TESTING ****');
 
 mp = new RequestPool
 
-
-
 thisNum = Math.random();
 let thisAddress1 = SHA256(thisNum.toString()).toString();
 console.log(`Test address1: ${thisAddress1}`);
@@ -91,7 +83,6 @@ console.log(`Test address1: ${thisAddress1}`);
 thisNum = Math.random();
 let thisAddress2 = SHA256(thisNum.toString()).toString();
 console.log(`Test address2: ${thisAddress2}`);
-
 
 
 // For timestep simulation
@@ -105,7 +96,6 @@ console.log('\n\n');
 var interval = setInterval(function () {
     if (currStep <= timesteps) {
 
-
         if (currStep === 2) {
             mp.addRequest(thisAddress1)
         } else if (currStep === 4){
@@ -117,6 +107,12 @@ var interval = setInterval(function () {
         // console.log(`Mempool: ${JSON.stringify(mp.mempool)}`);
         // console.log(`Mempool: ${mp.mempool.keys()}`);
         console.log(`ts${currStep} Mempool with ${Object.keys(mp.mempool).length} keys: ${Object.keys(mp.mempool)}`);
+        
+        for (address in mp.mempool) {
+            console.log(mp.mempool[address].respond())
+         }
+
+
         currStep++;
     }
     else {
