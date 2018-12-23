c0ca3b7c6b215e3fc041d701639b113aafece8dd
MarcusJones <jones.0bj3@gmail.com>
2018-12-23 15:15:47 +0100

    add two dictionaries for request, and timer

--- PROJECT4_star_notary/server/controllers/MempoolRequestValidator.js
+++ PROJECT4_star_notary/server/controllers/MempoolRequestValidator.js
@@ -1,17 +1,26 @@
-// Date
-// const TimeoutRequestsWindowTime = 5*60*1000;
-const TimeoutRequestsWindowTime = 5;
+const SHA256 = require('crypto-js/sha256');
 
-// timer = 
-//
+// const TimeoutRequestsWindowTime = 5*60*1000;
+const TimeoutRequestsWindowTime = 5*1000;
+console.log(`Request window: ${TimeoutRequestsWindowTime} s`);
+
+
+// let d = Date.now()
+// console.log(d);
+class Request {
+    constructor(data) {
+      this.requestTime = Date.now();
+
+
+    }
+  }
+
 class RequestPool {
     constructor() {
         // A simple dict with address : request time
         this.mempool = {};
-        // this.timeoutRequests  = [];
-        this.addRequest();
-        this.removeValidationRequest();
-
+        this.timeoutRequests = {};
+        // this.removeValidationRequest();
     }
 
     requestVaidation(address) {
@@ -23,16 +32,24 @@ class RequestPool {
 
     removeValidationRequest(address) {
         delete this.mempool[address]
+        delete this.timeoutRequests[address]
         console.log(`Removed ${address} from mempool`);
         
     }
 
     addRequest(address) {
         var self = this // Need to keep the instance in scope, this is not in scope!
-        this.mempool[address] = setTimeout(
+
+        this.mempool[address] = new Request;
+        // Add a countdown to this address key
+        this.timeoutRequests[address] = setTimeout(
             function () {
                 self.removeValidationRequest(address)
             }, TimeoutRequestsWindowTime);
+
+
+        console.log(`Added ${address} to mempool`);
+        
 
     }
 
@@ -46,61 +63,44 @@ class RequestPool {
 
 }
 
-// 
+console.log('\n\n');
+console.log('**** TESTING ****');
 
 mp = new RequestPool
-// mp.requestVaidation("asdfasdf")
-console.log(mp.timeoutRequests);
-// setTimeout()
-
-// function checkPool() {
-//     console.log('Mempool:');
-
-//     console.log(mp.mempool);
-// }
-
-//  setTimeout(function() {
-//     checkPool();
-//  }, 1000); //milliseconds
 
 
-
-const SHA256 = require('crypto-js/sha256');
-let d = Date.now()
-console.log(d);
 
 thisNum = Math.random();
 let thisAddress1 = SHA256(thisNum.toString()).toString();
-console.log(`A1: ${thisAddress1}`);
+console.log(`Test address1: ${thisAddress1}`);
 
 thisNum = Math.random();
 let thisAddress2 = SHA256(thisNum.toString()).toString();
-console.log(`A2: ${thisAddress2}`);
+console.log(`Test address2: ${thisAddress2}`);
 
 
 
 // For timestep simulation
-var timesteps = 5;
-var time = 1;
+var timesteps = 20;
 var delaytime = 1000; // milliseconds
+var currStep = 1;
 
+console.log('\n\n');
 
-
-console.log(`Mempool: ${mp.mempool}`);
+// console.log(`Mempool: ${mp.mempool}`);
 
 var interval = setInterval(function () {
-    if (time <= timesteps) {
-        if (time === 2) {
+    if (currStep <= timesteps) {
+        if (currStep === 2) {
             mp.addRequest(thisAddress1)
         }
-        console.log(`Timestep ${time}`);
-
+        // console.log(`Timestep ${currStep}`);
         // console.log(`Mempool: ${JSON.stringify(mp.mempool)}`);
         // console.log(`Mempool: ${mp.mempool.keys()}`);
-        console.log(`Mempool with ${Object.keys(mp.mempool).length} keys: ${Object.keys(mp.mempool)}`);
+        console.log(`ts${currStep} Mempool with ${Object.keys(mp.mempool).length} keys: ${Object.keys(mp.mempool)}`);
 
 
-        time++;
+        currStep++;
     }
     else {
         clearInterval(interval);
