112f5e84e1c00f8c5e3f535acbde0aaa58b0716c
MarcusJones <jones.0bj3@gmail.com>
2018-12-30 13:27:07 +0100

    update Web3 with index.html

--- TEST_ganache_web3/app.js
+++ TEST_ganache_web3/app.js
@@ -1,7 +1,57 @@
-var Web3 = require('web3')
+/*##########################
+##     CONFIGURATION      ##
+##########################*/
 
-var url = 'HTTP://127.0.0.1:7545' // 8545 if using ganache-cli
+//  -- Step 1: Set up the appropriate configuration
+var Web3 = require("web3")
+var EthereumTransaction = require("ethereumjs-tx")
+var web3 = new Web3('HTTP://127.0.0.1:7545')
 
-var web3 = new Web3(url)
+// -- Step 2: Set the sending and receiving addresses for the transaction.
+var sendingAddress = '0x15a61afcC45149e595Cc177082d8e0970Fb9b4Ae'
+var receivingAddress = '0x03bDDDB36a858bA0714ce1522c8121fB4d0FC93B'
 
-web3.eth.getAccounts().then(accounts => console.log(accounts));
+// -- Step 3: Check the balances of each address
+web3.eth.getBalance(sendingAddress).then(console.log)
+web3.eth.getBalance(receivingAddress).then(console.log)
+
+/*##########################
+##  CREATE A TRANSACTION  ##
+##########################*/
+
+// -- Step 4: Set up the transaction using the transaction variables as shown
+var rawTransaction = {
+    nonce: 1,
+    to: receivingAddress,
+    gasPrice: 20000000,
+    gasLimit: 30000,
+    value: 10000000,
+    data: ""
+}
+
+// -- Step 5: View the raw transaction
+rawTransaction
+
+// -- Step 6: Check the new account balances (they should be the same)
+web3.eth.getBalance(sendingAddress).then(console.log)
+web3.eth.getBalance(receivingAddress).then(console.log)
+
+// Note: They haven't changed because they need to be signed...
+
+/*##########################
+##  Sign the Transaction  ##
+##########################*/
+
+// -- Step 7: Sign the transaction with the Hex value of the private key of the sender
+var privateKeySender = '50aecda1ec0d2aad06bf49a644e34ee32186b9870a8eedb3a15da52565f5c7df'
+var privateKeySenderHex = new Buffer(privateKeySender, 'hex')
+var transaction = new EthereumTransaction(rawTransaction)
+transaction.sign(privateKeySenderHex)
+
+/*#########################################
+##  Send the transaction to the network  ##
+#########################################*/
+
+// -- Step 8: Send the serialized signed transaction to the Ethereum network.
+var serializedTransaction = transaction.serialize();
+web3.eth.sendSignedTransaction(serializedTransaction);
