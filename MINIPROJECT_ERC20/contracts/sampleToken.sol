48b7e861b1817ab393ee587b8b42e98598042463
MarcusJones <jones.0bj3@gmail.com>
2019-01-01 23:03:26 +0100

    refactor formatting

--- MINIPROJECT_ERC20/contracts/sampleToken.sol
+++ MINIPROJECT_ERC20/contracts/sampleToken.sol
@@ -1,15 +1,16 @@
 pragma solidity ^0.4.24;
+
 // import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./MyERC20.sol";
-contract sampleToken is ERC20 
-{
-string public name = "ExampleToken";
-string public symbol = "EGT";
-uint public decimals = 18;
-uint public INITIAL_SUPPLY = 10000 * (10 ** decimals);
-constructor() public 
-    {
-_totalSupply = INITIAL_SUPPLY;
-_balances[msg.sender] = INITIAL_SUPPLY;
+
+contract sampleToken is ERC20 {
+    string public name = "ExampleToken";
+    string public symbol = "EGT";
+    uint public decimals = 18;
+    uint public INITIAL_SUPPLY = 10000 * (10 ** decimals);
+
+    constructor() public {
+        _totalSupply = INITIAL_SUPPLY;
+        _balances[msg.sender] = INITIAL_SUPPLY;
     }
 }
