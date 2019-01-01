395d11b8059d1d260e3cf5e330e9b27161b77928
MarcusJones <jones.0bj3@gmail.com>
2019-01-01 11:43:52 +0100

    update

--- MINIPROJECT_ERC20/truffle-config.js
+++ MINIPROJECT_ERC20/truffle-config.js
@@ -66,7 +66,7 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
+    rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
