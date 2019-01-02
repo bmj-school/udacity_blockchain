216575c619f9f75f5ab7db3e3bb880950e10b544
MarcusJones <jones.0bj3@gmail.com>
2019-01-02 19:55:53 +0100

    fix Still broken.

--- MINIPROJECT_unbox_webpack/test/starNotary.js
+++ MINIPROJECT_unbox_webpack/test/starNotary.js
@@ -1,4 +1,4 @@
-// import 'babel-polyfill';
+import 'babel-polyfill';
 
 const StarNotary = artifacts.require('./StarNotary.sol')
 
@@ -10,27 +10,43 @@ contract('StarNotary', async (accs) => {
     accounts = accs;
     owner = accounts[0];
     instance = await StarNotary.deployed();
-    console.log('HELLO! THIS IS THE INSTANCE:');
-    // console.log(instance);
-    console.log('instance.starName' + instance.starName);
-    // console.log(await instance.starName.call()); 
-    
+    console.log('HELLO TEST');
+
+    StarNotary.deployed().then(function (instance) {
+        return instance.starName.call();
+        }).then(function (starName) { console.log('TESTING STAR NAME:' + starName);
+    });
+
+    instance.then(function (instance) {
+        return instance.starName.call();
+        }).then(function (starName) { console.log('TESTING STAR NAME 2:' + starName);
+    });
+
 })
 
 it('has correct name', async () => {
-    console.log('HELLO, inside test.');
-    assert.equal(await instance.starName.call(), 'Awesome Udacity Star');
+    val = await instance.starName.call();
+    console.log('HHHHHHHHHH');
+    
+    assert.equal(val, 'Awesome Udacity Star');
 })
 
-it('can be claimed', async () => {
-    await instance.claimStar({ from: owner });
-    assert.equal(await instance.starOwner.call(), owner)
-})
 
-it('can change owners', async () => {
-    var secondUser = accounts[1];
-    await instance.claimStar({ from: owner })
-    assert.equal(await instance.starOwner.call(), owner)
-    await instance.claimStar({ from: secondUser })
-    assert.equal(await instance.starOwner.call(), secondUser)
-})
+
+    // console.log('INSTANCE:');
+    // console.log(instance);
+    // console.log('instance.starName' + instance.starName);
+    // console.log(await instance.starName.call()); 
+
+// it('can be claimed', async () => {
+//     await instance.claimStar({ from: owner });
+//     assert.equal(await instance.starOwner.call(), owner)
+// })
+
+// it('can change owners', async () => {
+//     var secondUser = accounts[1];
+//     await instance.claimStar({ from: owner })
+//     assert.equal(await instance.starOwner.call(), owner)
+//     await instance.claimStar({ from: secondUser })
+//     assert.equal(await instance.starOwner.call(), secondUser)
+// })
--- MINIPROJECT_unbox_webpack/truffle.js
+++ MINIPROJECT_unbox_webpack/truffle.js
@@ -3,9 +3,9 @@ require('babel-register')
 
 module.exports = {
   networks: {
-    ganache: {
+    develop: {
       host: '127.0.0.1',
-      port: 7545,
+      port: 8545,
       network_id: '*' // Match any network id
     }
   },
