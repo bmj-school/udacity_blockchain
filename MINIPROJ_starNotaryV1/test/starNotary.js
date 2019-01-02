26df9fda20521ed97a6468648894a88e5cfcc969
MarcusJones <jones.0bj3@gmail.com>
2019-01-02 15:46:28 +0100

    update Test finished

--- MINIPROJ_starNotaryV1/test/starNotary.js
+++ MINIPROJ_starNotaryV1/test/starNotary.js
@@ -1,2 +1,18 @@
+import 'babel-polyfill';
+import { AssertionError } from 'assert';
+
 const StarNotary = artifacts.require('./starNotary.sol')
 
+let instance;
+let accounts;
+var owner;
+
+contract('StarNotary', async (accs) => {
+    accounts = accs;
+    owner = accounts[0];
+    instance = await  StarNotary.deployed();
+})
+
+it('has the correct name', async () => {
+    assert.equal(await instance.starName.call(), 'Awesome Udacity Star');
+})
