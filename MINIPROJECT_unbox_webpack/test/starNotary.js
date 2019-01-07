92df3d76323b511af28ec0ff7924a4c3610abeca
MarcusJones <jones.0bj3@gmail.com>
2019-01-07 20:30:04 +0100

    update

--- MINIPROJECT_unbox_webpack/test/starNotary.js
+++ MINIPROJECT_unbox_webpack/test/starNotary.js
@@ -7,28 +7,12 @@ let accounts;
 var owner;
 
 contract('StarNotary', async (accs) => {
-    accounts = accs;
-    owner = accounts[0];
-    instance = await StarNotary.deployed();
-    console.log('HELLO TEST');
-
-    StarNotary.deployed().then(function (instance) {
-        return instance.starName.call();
-        }).then(function (starName) { console.log('TESTING STAR NAME:' + starName);
-    });
-
-    instance.then(function (instance) {
-        return instance.starName.call();
-        }).then(function (starName) { console.log('TESTING STAR NAME 2:' + starName);
-    });
-
+accounts = accs;
+owner = accounts[0];
+instance = await StarNotary.deployed();
 })
-
 it('has correct name', async () => {
-    val = await instance.starName.call();
-    console.log('HHHHHHHHHH');
-    
-    assert.equal(val, 'Awesome Udacity Star');
+    assert.equal(await instance.starName.call(), 'Awesome Udacity Star');
 })
 
 
