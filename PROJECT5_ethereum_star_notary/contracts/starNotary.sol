94fd16fbe3360f42427d4257f36ce81ee8e06371
MarcusJones <jones.0bj3@gmail.com>
2019-01-08 14:07:44 +0100

    update

--- PROJECT5_ethereum_star_notary/contracts/starNotary.sol
+++ PROJECT5_ethereum_star_notary/contracts/starNotary.sol
@@ -1,20 +1,36 @@
 pragma solidity ^0.4.23;
 
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+import 'https://github.com/MarcusJones/OpenZeppelin2/contracts/token/ERC721/ERC721.sol';
 
 contract StarNotary is ERC721 {
 
     struct Star {
         string name;
- 
+    }
+    
+    // An offer to exchange is proposed, and stored in a struct
+    // The following struct is over designed, it does not need so much information
+    // but for development purposes, it contains all required info!
+    struct ExchangeOffers {
+        uint256 IDtokenOffered;
+        uint256 IDtokenRequested;
+        address offeredBy;
+        address offeredTo;
+        uint256 expiry; // (Not implemented, but it would be useful to specify how long the offer stands!)
+        uint256 exchangePriceOffer; // (Not implemented, but you might want to offer additional ETH to incentivize someone to exchange)
+        uint256 exchangePriceRequest; // (Not implemented, but you might want to request additional ETH to incentivize yourself to exchange)
     }
     
     // CRITERIA: The smart contract tokens should have a name and a symbol.
     string public name = "Galaxy";
     string public symbol = "GLXY";
+    string public version = "4";
 
     mapping(uint256 => Star) public tokenIdToStarInfo;
     mapping(uint256 => uint256) public starsForSale;
+    // The smart contract acts as a trusted clearing house for ExchangeOffers
+    // The key of the mapping is the ID of the token requested (also stored in struct!) 
+    mapping(address => ExchangeOffers) public ExchangeClearingHouse;
 
     function createStar(string _name, uint256 _tokenId) public {
         Star memory newStar = Star(_name);
@@ -56,15 +72,53 @@ contract StarNotary is ERC721 {
 
 // Add a function called exchangeStars, so 2 users can exchange their star tokens...
 //Do not worry about the price, just write code to exchange stars between users.
+    
+    /* 
+    */
+    function offerExchange(uint256 _offeredTokenID, 
+                           uint256 _requestedTokenID,
+                           uint256 _timeOut,
+                           uint256 _exchangePriceOffer,
+                           uint256 exchangePriceRequest
+                           ) public {
+        require(_exists(_offeredTokenID));
+        require(_exists(_requestedTokenID));
+        
+        address offeredOwner = ownerOf(_offeredTokenID);
+        address requestedOwner = ownerOf(_requestedTokenID);
+        
+    }
+    
+    /* FOR DEVELOPMENT ONLY
+    For debugging, ensure 2 tokens exist and get their owners
+    */
+    function checkExchangeAddresses(uint256 _tokenOneID, uint256 _tokenTwoID) public view returns (address, address){
+        require(_exists(_tokenOneID));
+        require(_exists(_tokenTwoID));
+        
+        address address1 = ownerOf(_tokenOneID);
+        address address2 = ownerOf(_tokenTwoID);
+        
+        return(address1, address2);
+    }
 
-    function exchangeStarsNAIVE(uint256 _myTokenID, uint256 _theirTokenID) {
-        require(ownerOf(_myTokenID) == msg.sender);
-
-        address theirAddress = ownerOf(_theirTokenID);
-        // require(ownerOf(_theirTokenID) == _theirAddress);
-
-        safeTransferFrom(msg.sender, theirAddress, _myTokenID);
-        safeTransferFrom(theirAddress, msg.sender, _theirTokenID);
+    /* FOR DEVELOPMENT ONLY
+    FAILS, See note: `Requires the msg sender to be the owner, approved, or operator`
+    Super naive and insecure, for debugging
+    */
+    function exchangeStarsNAIVE(uint256 _tokenOneID, uint256 _tokenTwoID) public {
+        
+        require(_exists(_tokenOneID));
+        require(_exists(_tokenTwoID));
+        
+        require(msg.sender==ownerOf(_tokenOneID));
+        
+        address address1 = ownerOf(_tokenOneID);
+        address address2 = ownerOf(_tokenTwoID, {from: address2});
+        
+        transferFrom(address1, address2, _tokenOneID);
+        // The following line FAILS, since msg.sender != address2!!!
+        //transferFrom(address2, address1, _tokenOneID);
     }
 //
 
