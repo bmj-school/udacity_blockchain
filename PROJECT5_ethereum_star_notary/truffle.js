ac9da7d93fc01001a9dae822233f7b32cbcd73e0
MarcusJones <jones.0bj3@gmail.com>
2019-01-08 17:18:36 +0100

    add Load the seed phrase

--- PROJECT5_ethereum_star_notary/truffle.js
+++ PROJECT5_ethereum_star_notary/truffle.js
@@ -4,6 +4,12 @@ require('babel-register')
 // Edit truffle.config file should have settings to deploy the contract to the Rinkeby Public Network.
 // Infura should be used in the truffle.config file for deployment to Rinkeby.
 
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "2665f353b5e945559a1087ca832189a7";
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+
 module.exports = {
   networks: {
     ganache: {
