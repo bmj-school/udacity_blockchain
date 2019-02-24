c2d67500820676ee660e17d6e04383df5dd3c34f
MarcusJones <jones.0bj3@gmail.com>
2019-02-24 14:58:35 +0100

    add Boilerplate for lemonade demo project

---
+++ MINIPROJECT_lemonade_stand/LemonadeComments.sol
@@ -0,0 +1,18 @@
+/
+
+// Variable: Owner, skuCount
+// State: For Sale
+
+// Struct: Item: name, sky, price, state, seller, buyer
+// Mapping item->sku
+// Event: ForSale
+// Event: Sold
+// Modifier: Only Owner
+// Modifier: Verify caller
+// Modifier: Paid Enough
+// Modifier: For Sale
+// Modifier: sold
+// Function: Constructor
+// Function: Add Item
+// Function: Buy Item
+// Function: 
