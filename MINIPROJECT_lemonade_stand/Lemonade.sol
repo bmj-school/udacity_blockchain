777cc5ee3996f11ed9bf1883d8c96e66b14d7d40
MarcusJones <jones.0bj3@gmail.com>
2019-02-24 15:30:57 +0100

    update

--- MINIPROJECT_lemonade_stand/Lemonade.sol
+++ MINIPROJECT_lemonade_stand/Lemonade.sol
@@ -43,5 +43,49 @@ contract LemonadeStand {
         require(items[_sku].state == State.Sold);
         _;
     }
+    
+    constructor() public {
+        owner = msg.sender;
+        skuCount = 0;
+    }
+    
+    function addItem(string _name, uint _price) onlyOwner public {
+        skuCount = skuCount + 1;
+        emit ForSale(skuCount);
+        items[skuCount] = Item({
+            name: _name,
+            sku: skuCount,
+            price: _price,
+            state: State.ForSale,
+            seller: msg.sender,
+            buyer: 0
+        });
+    }
+    
+    function buyIteam(uint sku) forSale(sku) paidEnough(items[sku].price) public payable {
+        address buyer = msg.sender;
+        uint price = items[sku].price;
+        items[sku].buyer = buyer;
+        items[sku].state = State.Sold;
+        items[sku].seller.transfer(price);
+        emit Sold(sku);
+    }
+    
+    function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, string stateIs, address seller, address buyer) {
+        uint state;
+        name = items[_sku].name;
+        sku = items[_sku].sku;
+        price = items[_sku].price;
+        state = uint(items[_sku].state);
+        if( state == 0 ) {
+            stateIs = "For Sale";
+        }
+        if( state == 1 ) {
+            stateIs = "Sold";
+        }
+        
+        seller = items[_sku].seller;
+        buyer = items[_sku].buyer;
+    }
 }
 
