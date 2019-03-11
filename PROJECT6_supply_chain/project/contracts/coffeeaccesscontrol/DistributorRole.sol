4c2f8d5a019fdeeb76f64cf5f099f554b94a2e2a
MarcusJones <jones.0bj3@gmail.com>
2019-03-11 13:20:43 +0100

    update Distributor

--- PROJECT6_supply_chain/project/contracts/coffeeaccesscontrol/DistributorRole.sol
+++ PROJECT6_supply_chain/project/contracts/coffeeaccesscontrol/DistributorRole.sol
@@ -5,19 +5,23 @@ import "./Roles.sol";
 
 // Define a contract 'DistributorRole' to manage this role - add, remove, check
 contract DistributorRole {
+  using Roles for Roles.Role;
 
   // Define 2 events, one for Adding, and other for Removing
+  event DistributorAdded(address indexed account);
+  event DistributorRemoved(address indexed account);
 
   // Define a struct 'distributors' by inheriting from 'Roles' library, struct Role
+  Roles.Role private distributors;
 
   // In the constructor make the address that deploys this contract the 1st distributor
   constructor() public {
-
+    _addDistributor(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyDistributor() {
-
+    require(isDistributor(msg.sender));
     _;
   }
 
