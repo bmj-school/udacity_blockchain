c47876d4e9a0d20aec6e072705ba9cd10f937c9c
MarcusJones <jones.0bj3@gmail.com>
2019-05-06 10:45:50 +0200

    add account loggign

--- PROJECT7_flight_surety/config/testConfig.js
+++ PROJECT7_flight_surety/config/testConfig.js
@@ -14,7 +14,7 @@ var BigNumber = require('bignumber.js');
 var Config = async function(accounts) {
     log('Preparing test configuration environment...')
 
-     
+
     // These test addresses are useful when you need to add
     // multiple users in test scripts
     // let testAddresses = [
@@ -29,6 +29,8 @@ var Config = async function(accounts) {
     //     "0x2f2899d6d35b1a48a4fbdc93a37a72f264a9fca7"
     // ];
     let owner = "0x627306090abab3a6e1400e9345bc60c78a8bef57";
+    log(`\t owner: ${owner}`)
+
     let testAirlineAccounts = [
         "0xf17f52151ebef6c7334fad080c5704d77216b732",
         "0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef",
@@ -36,19 +38,25 @@ var Config = async function(accounts) {
         "0x0d1d4e623d10f9fba5db95830f7d3839406c6af2",
         "0x2932b7a2355d6fecc4b5c0b6bd44cc31df247a2e"
     ];
+    for (addr of testAirlineAccounts) {
+        log(`\t airlines: ${addr}`)
+    }
 
     let testOtherAccounts = [
         "0x2191ef87e392377ec08e7c08eb105ef5448eced5",
         "0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5",
         "0x6330a553fc93768f612722bb8c2ec78ac90b3bbc",
-        "0x5aeda56215b167893e80b4fe645ba6d5bab767de"    
-    ];    
+        "0x5aeda56215b167893e80b4fe645ba6d5bab767de"
+    ];
+    for (addr of testOtherAccounts) {
+        log(`\t other: ${addr}`)
+    }
 
     let flightSuretyData = await FlightSuretyData.new('Genesis Air', testAirlineAccounts[0]);
     let flightSuretyApp = await FlightSuretyApp.new();
     log(`flightSuretyApp = ${flightSuretyApp}`)
     log(`flightSuretyData = ${flightSuretyData}`)
-    
+
 
     log('Test configuration environment loaded.');
     return {
